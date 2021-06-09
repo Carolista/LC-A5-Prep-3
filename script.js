646904af223389ac99527a504dea7e4bc2f6ab61
Carolista <cjones.renaissancewoman@gmail.com>
2021-06-09 01:25:06 -0500

    Tested display of results by fetching all drinks with gin and showing their names in a list.

--- script.js
+++ script.js
@@ -7,6 +7,57 @@ let categories = [];
 let ingredients = [];
 let drinks = [];
 
+const baseURL = "https://www.thecocktaildb.com/api/json/v1/1/";
+
+const endpoints = {
+    name: baseURL + "search.php?s=",
+    ingredient: baseURL + "filter.php?i=",
+    type: baseURL + "filter.php?a=",
+    category: baseURL + "filter.php?c=",
+}
+
+
+
+function init() {
+    
+    // CREATE OBJECTS FROM HTML ELEMENTS
+    const categorySelect = document.getElementById("category");
+    const ingredientSelect = document.getElementById("ingredient");
+    const submitButton = document.getElementById("submit");
+    const resetButton = document.getElementById("reset");
+    const resultsArea = document.getElementById("results-area");
+
+    // POPULATE DROPDOWN INPUTS
+    categorySelect.innerHTML = setCategoryOptions();
+    ingredientSelect.innerHTML = setIngredientOptions();
+
+    // LISTEN FOR EVENTS
+    submitButton.addEventListener("click", (event) => {
+        fetchDrinks("ingredient", "gin");
+        setTimeout(() => {
+            resultsArea.innerHTML = displayResults(); 
+        }, 300);
+        event.preventDefault();
+    });
+    resetButton.addEventListener("click", (event) => {
+        drinks = [];
+        resultsArea.innerHTML = `
+            <p id="no-results">No results to display.</p>
+        `;
+    });
+
+    // SUPPORT DYNAMIC VIEW
+    function displayResults() {
+        let results = "";
+        drinks.forEach(drink => {
+            results += `
+                <p>${drink.name}</p>
+            `;
+        });
+        return results;
+    }
+}
+
 function fetchCategories() {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list").then( function(response) {
         response.json().then( function(json) {
@@ -29,15 +80,6 @@ function fetchIngredients() {
             init(); 
         });
     });
-}
-
-const baseURL = "https://www.thecocktaildb.com/api/json/v1/1/";
-
-const endpoints = {
-    name: baseURL + "search.php?s=",
-    ingredient: baseURL + "filter.php?i=",
-    type: baseURL + "filter.php?a=",
-    category: baseURL + "filter.php?c=",
 }
 
 function fetchDrinks(searchType, keyword) {
@@ -68,7 +110,7 @@ function fetchDrinks(searchType, keyword) {
                     type: drink.strAlcoholic,
                     glass: drink.strGlass,
                     directions: drink.strInstructions,
-                    ingreds: ingredientList,
+                    ingredients: ingredientList,
                     photo: drink.strDrinkThumb
                 }
             });
@@ -77,46 +119,26 @@ function fetchDrinks(searchType, keyword) {
     });
 }
 
-
-
-function init() {
-    
-    // CREATE OBJECTS FROM HTML ELEMENTS
-    const categorySelect = document.getElementById("category");
-    const ingredientSelect = document.getElementById("ingredient");
-    const submitButton = document.getElementById("submit");
-    const resultsArea = document.getElementById("results-area");
-
-    function setCategoryOptions() {
-        let options = `
-            <option value="any">Any</option>
+function setCategoryOptions() {
+    let options = `
+        <option value="any">Any</option>
+    `;
+    for (let i=0; i < categories.length; i++) {
+        options += `
+            <option value="${categories[i].toLowerCase()}">${categories[i]}</option>
         `;
-        for (let i=0; i < categories.length; i++) {
-            options += `
-                <option value="${categories[i].toLowerCase()}">${categories[i]}</option>
-            `
-        }
-        return options;
     }
+    return options;
+}
 
-    function setIngredientOptions() {
-        let options = `
-            <option value="any">Any</option>
+function setIngredientOptions() {
+    let options = `
+        <option value="any">Any</option>
+    `;
+    for (let i=0; i < ingredients.length; i++) {
+        options += `
+            <option value="${ingredients[i].toLowerCase()}">${ingredients[i]}</option>
         `;
-        for (let i=0; i < ingredients.length; i++) {
-            options += `
-                <option value="${ingredients[i].toLowerCase()}">${ingredients[i]}</option>
-            `
-        }
-        return options;
     }
-
-    categorySelect.innerHTML = setCategoryOptions();
-    ingredientSelect.innerHTML = setIngredientOptions();
-
-    submitButton.addEventListener("click", (event) => {
-        // fetchDrinks("ingredient", "gin");
-        event.preventDefault();
-    });
-
+    return options;
 }
