1a6a4cdf499a28479e0190858b756684ad5c5bee
Carolista <cjones.renaissancewoman@gmail.com>
2021-06-10 02:49:30 -0500

    finished most of basic styling for page at full width

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 window.addEventListener("load", function() {
-    fetchCategories();
-    // Note: init() is called from fetchIngredients() to make sure the data needed to populate the form inputs has already arrived before the page is rendered.
+    fetchDrinks();
+    // Note: init() is called from fetchIngredients() to make sure fetched data has returned from the API before the page is rendered. All fetch functions are chained this way.
 });
 
 let categories = [];
 let ingredients = [];
 let allDrinks = [];
+let currentDrinks = [];
 
-const baseURL = "https://www.thecocktaildb.com/api/json/v1/1/";
-
-const endpoints = {
-    letter: baseURL + "search.php?f=",
-    name: baseURL + "search.php?s=",
-    ingredient: baseURL + "filter.php?i=",
-    type: baseURL + "filter.php?a=",
-    category: baseURL + "filter.php?c=",
-}
+// TODO: Create splash screen effect to fade to white while data is loading ?
+// TODO: Consider nixing ingredients dropdown and just make it part of keyword search
 
 function init() {
+
+    /** ALPHABETIZE DRINKS BY NAME **/
+    // FIXME: Consider randomizing instead if still not working, or sort each time submit is clicked
+    allDrinks.sort((a,b) => {return a.name - b.name});
+    console.log("All drinks sorted.");
     
     /** CREATE OBJECTS FROM HTML ELEMENTS **/
-    const categorySelect = document.getElementById("category");
-    const ingredientSelect = document.getElementById("ingredient");
-    const submitButton = document.getElementById("submit");
-    const resetButton = document.getElementById("reset");
+    const keywordInput = document.getElementById("keyword-input");
+    const typeInput = document.querySelector("input[name=type-input]")
+    const categorySelect = document.getElementById("category-input");
+    const ingredientSelect = document.getElementById("ingredient-input");
+    const submitButton = document.getElementById("submit-button");
+    const resetButton = document.getElementById("reset-button");
     const resultsArea = document.getElementById("results-area");
 
     /** POPULATE DROPDOWN INPUTS **/
@@ -32,87 +33,104 @@ function init() {
 
     /** LISTEN FOR EVENTS **/
     submitButton.addEventListener("click", (event) => {
-        fetchDrinks(); // temp
-        setTimeout(() => {
-            // console.log(drinks);
-            resultsArea.innerHTML = displayResults(); 
-            // console.log(drinks);
-        }, 1000);
+        // FIXME: temporarily set currentDrinks to allDrinks
+        currentDrinks = allDrinks.slice();  
+        // TODO: add logic to filter drinks acc to settings
+        resultsArea.innerHTML = displayResults(); 
         event.preventDefault();
     });
     resetButton.addEventListener("click", (event) => {
-        drinks = [];
+        currentDrinks = [];
         resultsArea.innerHTML = `
-            <p id="no-results">No results to display.</p>
+            <div id="no-results">
+                <img id="empty-glass" src="images/empty-glass.png" />
+                <p class="no-results">No results to display.</p>
+                <p class="no-results">Try a new search!</p>
+            </div>
         `;
     });
 
     /** USE FORM INPUT CRITERIA TO GET AND DISPLAY RESULTS **/
 
-    // Do a single search to a specific endpoint depending on criterion
-    function fetchDrinks() {
-        let alpha = "abcdefghijklmnopqrstuvwxyz";
-        allDrinks = [];
-        for (let a=0; a < alpha.length; a++) {
-            let fullURL = endpoints["letter"] + alpha[a];
-            fetch(fullURL).then( function(response) {
-                response.json().then( function(json) {
-                    let drinkObjects = json.drinks;
-                    let drinksByLetter = [];
-                    if (drinkObjects !== null) {
-                        drinksByLetter = drinkObjects.map(drink => {
-                            // First handle the 30 separate properties for ingredients and measures
-                            let ingredientList = [];
-                            for (let i=0; i < 15; i++) {
-                                let num = String(i+1);
-                                let ing = drink["strIngredient" + num];
-                                let msr = drink["strMeasure" + num];
-                                if (ing !== null) {
-                                    // console.log("ing is " + ing);
-                                    if (msr !== null) {
-                                        // console.log("msr is " + msr)
-                                        ingredientList.push(`${ing}, ${msr.trim()}`);
-                                    } else {
-                                        ingredientList.push(`${ing}`);
-                                    }
-                                } else {
-                                    break;
-                                }
-                            }
-                            // Then create the new object to be mapped to the drinks array
-                            return {
-                                name: drink.strDrink,
-                                category: drink.strCategory,
-                                type: drink.strAlcoholic,
-                                glass: drink.strGlass,
-                                ingredients: ingredientList,
-                                directions: drink.strInstructions,
-                                photo: drink.strDrinkThumb,
-                            }
-                        });
-                    }
-                    allDrinks = [...allDrinks, ...drinksByLetter];
-                    // console.log(drinksByLetter.length + " drinks added for letter " + alpha[a]);
-                });
-            });
-        }
-    }
+    
 
     function displayResults() {
         let results = "";
-        allDrinks.forEach(drink => {
+        let color, listItems;
+        currentDrinks.forEach(drink => {
+            color = (drink.type.toLowerCase() === "alcoholic" ? "alcohol" : "no-alcohol");
+            listItems = "";
+            drink.ingredients.map(ingredient => listItems += `<li>${ingredient}</li>`);
             results += `
-                <div class="recipe-block">
-                    <h3>${drink.name}</h3>
-                    <p>${drink.category} | ${drink.type}</p>
-                    <p>Enjoy your ${drink.name} in a ${drink.glass}</p>
-                    <p></p>
+                <div class="recipe-card">
+                    <div class="recipe-card-left">
+                        <img class="drink" src="${drink.image}" />
+                    </div>
+                    <div class="color-bar-1 ${color}"></div>
+                    <div class="recipe-card-right">
+                        <h3>${drink.name}</h3>
+                        <p class="info">${drink.category} &bull; ${drink.type}</p>
+                        <p class="glass">Enjoy your ${drink.name} in a <span class="capitalize">${drink.glass}</span>.</p>
+                        <h5>Ingredients</h5>
+                        <ul>${listItems}</ul>
+                        <h5>Directions</h5>
+                        <p class="directions">${drink.directions}</p>
+                    </div>
+                    <div class="color-bar-2 ${color}"></div>
                 </div>
             `;
         });
-        console.log(allDrinks.length + " drinks total");
         return results;
     }
+}
+
+function fetchDrinks() {
+    let alpha = "abcdefghijklmnopqrstuvwxyz";
+    allDrinks = [];
+    for (let a=0; a < alpha.length; a++) {
+        let fullURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=" + alpha[a];
+        fetch(fullURL).then( function(response) {
+            response.json().then( function(json) {
+                let drinkObjects = json.drinks;
+                let drinksByLetter = [];
+                if (drinkObjects !== null) {
+                    drinksByLetter = drinkObjects.map(drink => {
+                        // First handle the 30 separate properties for ingredients and measures
+                        let ingredientList = [];
+                        for (let i=0; i < 15; i++) {
+                            let num = String(i+1);
+                            let ing = drink["strIngredient" + num];
+                            let msr = drink["strMeasure" + num];
+                            if (ing !== null && ing !== "") {
+                                // console.log("ing is " + ing);
+                                if (msr !== null && msr !== "") {
+                                    // console.log("msr is " + msr)
+                                    ingredientList.push(`${ing}, ${msr.trim()}`);
+                                } else {
+                                    ingredientList.push(`${ing}`);
+                                }
+                            } else {
+                                break;
+                            }
+                        }
+                        // Then create the new object to be mapped to the drinks array
+                        return {
+                            name: drink.strDrink,
+                            category: drink.strCategory,
+                            type: drink.strAlcoholic,
+                            glass: drink.strGlass,
+                            ingredients: ingredientList,
+                            directions: drink.strInstructions,
+                            image: drink.strDrinkThumb,
+                        }
+                    });
+                }
+                allDrinks = [...allDrinks, ...drinksByLetter];
+            });
+        });
+    }
+    console.log("Drinks loaded.");
+    fetchCategories();
 }
 
 // Get all possible categories from API, needed for Category dropdown input
@@ -136,7 +154,9 @@ function fetchIngredients() {
             ingredients = ingredientsObjects.map(ingredient => ingredient.strIngredient1);
             ingredients.sort();
             console.log("Ingredients loaded.");
-            init(); 
+            setTimeout(() => {
+                init(); 
+            }, 500);
         });
     });
 }
